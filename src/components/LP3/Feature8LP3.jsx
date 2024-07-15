eecb639918f084bf14d0a8d94576f2f8282f4030
Mohammed Saud Jaafar <saudjafar71@gmail.com>
2024-07-16 04:26:11 +0530

    LP8 added

--- src/components/LP3/Feature8LP3.jsx
+++ src/components/LP3/Feature8LP3.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import dotsLP3F8 from '../../assets/svgs/dots-LP3F8.svg'
+import arrowSVG from '../../assets/svgs/arrow-circleBG.svg';
 const Feature8LP3 = () => {
     return (
         <section className='feature-8-LP3-section'>
@@ -8,6 +9,11 @@ const Feature8LP3 = () => {
                 <h1 className='header'>
                     LEARN HOW TO SAVE UP TO 70% ON YOUR CX.  DOWNLOAD OUR FREE GUIDE NOW.
                 </h1>
+
+                <div className="input-container">
+                    <input type="email" placeholder="your@email.com" className="custom-input" />
+                    <img src={arrowSVG} alt="Arrow" className="input-arrow" />
+                </div>
             </div>
         </section>
     )
